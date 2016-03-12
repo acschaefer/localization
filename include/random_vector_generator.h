3ba7e9694f0effe9b89c37bf2c668293df67b6b2
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-03-12 12:56:48 +0100

    Add class for random pose generation.

--- include/random_vector_generator.h
+++ include/random_vector_generator.h
@@ -25,7 +25,7 @@ protected:
 
 
 protected:
-    std::vector<RandomNumberGenerator> random_number_generators_;
+    std::vector<RandomNumberGenerator> number_generators_;
 
 
 public:
@@ -34,8 +34,8 @@ public:
         for (int row = 0; row < Dim; row++)
         {
             NormalDistribution normal_distribution(mean[row], var[row]);
-            RandomNumberGenerator random_number_generator(boost::mt19937(time(NULL)), normal_distribution);
-            random_number_generators_.push_back(random_number_generator);
+            RandomNumberGenerator number_generator(boost::mt19937(time(NULL)), normal_distribution);
+            number_generators_.push_back(number_generator);
         }
     }
 
@@ -45,7 +45,7 @@ public:
         Vector vector;
 
         for (int row = 0; row < Dim; row++)
-            vector[row] = random_number_generators_[row]();
+            vector[row] = number_generators_[row]();
 
         return vector;
     }
@@ -55,21 +55,48 @@ public:
 class RandomAngleAxisGenerator : protected RandomVectorGenerator<3>
 {
 protected:
-    RandomNumberGenerator random_number_generator_angle_;
+    RandomNumberGenerator number_generator_angle_;
 
 
 public:
     RandomAngleAxisGenerator(double angle_mean, double angle_var,
                              const Eigen::Vector3d& axis_mean, const Eigen::Vector3d& axis_var)
      : RandomVectorGenerator<3>(axis_mean, axis_var), 
-       random_number_generator_angle_(boost::mt19937(time(NULL)), NormalDistribution(angle_mean, angle_var))
+       number_generator_angle_(boost::mt19937(time(NULL)), NormalDistribution(angle_mean, angle_var))
     {
     }	
 
 
     Eigen::AngleAxisd generate_angle_axis()
     {
-	return Eigen::AngleAxisd(random_number_generator_angle_(), generate_vector().normalized());
+	return Eigen::AngleAxisd(number_generator_angle_(), generate_vector().normalized());
+    }
+};
+
+
+class RandomPoseGenerator  
+{
+protected:
+    RandomVectorGenerator<3> vector_generator_;
+    RandomAngleAxisGenerator angle_axis_generator_;
+
+
+public:
+    RandomPoseGenerator(const Eigen::Vector3d& position_mean, const Eigen::Vector3d& position_var,
+                        double angle_mean, double angle_var,
+                        const Eigen::Vector3d& axis_mean, const Eigen::Vector3d& axis_var)
+     : vector_generator_(position_mean, position_var),
+       angle_axis_generator_(angle_mean, angle_var, axis_mean, axis_var)
+    {
+    }
+
+
+    Eigen::Isometry3d generate_pose()
+    {
+        return Eigen::Isometry3d().fromPositionOrientationScale(
+                    vector_generator_.generate_vector(), 
+                    angle_axis_generator_.generate_angle_axis(),
+                    Eigen::Vector3d(1.0, 1.0, 1.0));
     }
 };
 
--- src/test_random_generator.cpp
+++ src/test_random_generator.cpp
@@ -4,10 +4,10 @@
 
 int main(int argc, char** argv)
 {
-    Eigen::Vector3d mean;
-    Eigen::Vector3d var;
-    var[0] = var[1] = var[2] = 1.0;
-    RandomVectorGenerator<3> vector_generator(mean, var);
+    Eigen::Vector3d position_mean;
+    Eigen::Vector3d position_var;
+    position_var[0] = position_var[1] = position_var[2] = 1.0;
+    RandomVectorGenerator<3> vector_generator(position_mean, position_var);
     
     for (int i = 0; i < 100; i++)
     {
@@ -32,6 +32,20 @@ int main(int argc, char** argv)
         Eigen::AngleAxisd a = angle_axis_generator.generate_angle_axis();
 	std::cout << "[" << a.axis()[0] << "; " << a.axis()[1] << "; " << a.axis()[2] << "], " 
 	          << a.angle() << std::endl;
+    }
+
+    std::cout << std::endl 
+              << "##########################################################" 
+              << std::endl << std::endl;
+
+    RandomPoseGenerator pose_generator(position_mean, position_var, 
+                                       angle_mean, angle_var,
+                                       axis_mean, axis_var);
+
+    for (int i = 0; i < 100; i++)
+    {
+        Eigen::Isometry3d pose = pose_generator.generate_pose();
+        std::cout << "[" << pose(0, 3) << "; " <<  pose(1, 3) << "; " << pose(2, 3) << "]" << std::endl;
     }
 
     return 0;
