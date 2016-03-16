4676a564d25854194358aba8b86c4c790977e856
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-03-16 08:37:33 +0100

    Add abstract method to class SensorModel.

--- localizer/include/sensor_model.h
+++ localizer/include/sensor_model.h
@@ -1,11 +1,18 @@
 #ifndef SENSOR_MODEL_H_
 #define SENSOR_MODEL_H_ SENSOR_MODEL_H_
 
+#include <particle.h>
+
 
 class SensorModel
 {
 public:
     typedef int Measurement;
+
+
+public:
+    virtual void compute_weights(const Measurement& measurement,
+                                 std::vector<Particle>& particles) = 0;
 };
 
 
