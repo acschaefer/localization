2abd56089215f507d622e41b4eb564a3fee5610f
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-03-16 14:10:11 +0100

    Document class Particle.

--- localizer/include/particle.h
+++ localizer/include/particle.h
@@ -1,42 +1,56 @@
 #ifndef PARTICLE_H_
 #define PARTICLE_H_ PARTICLE_H_
 
+// ROS coordinate transformations.
 #include <tf/tf.h>
 
 
+/// Particle class for use with the ParticleFilter class for robot localization.
+/// Stores all the information relevant for a particle of particle filter
+/// used for robot localization: pose and weight.
 class Particle
 {
 protected:
+    /// Robot pose.
     tf::Transform pose_;
+
+    /// Weight of the particle.
     double weight_;
 
 
 public:
+    /// Constructor.
+    /// Initializes the particle to the given pose and weight.
     Particle(const tf::Transform& pose = tf::Transform::getIdentity(), double weight = 1.0)
     {
-        pose_   = pose;
-        weight_ = weight;
+        set_pose(pose);
+        set_weight(weight);
     }
 
 
+    /// Sets the particle's pose.
     void set_pose(const tf::Transform& pose = tf::Transform::getIdentity())
     {
         pose_ = pose;
     }
 
 
+    /// Returns the particle's pose.
     tf::Transform get_pose() const
     {
         return pose_;
     }
 
 
+    /// Sets the particles weight.
+    /// \param[in] weight particle weight. Negative weights are set to zero.
     void set_weight(double weight)
     {
-        weight_ = weight;
+        weight_ = std::max(std::numeric_limits<double>::epsilon(), weight);
     }
 
 
+    /// Returns the particle's weight.
     double get_weight() const
     {
         return weight_;
