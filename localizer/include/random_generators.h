dcd93d247a555e5a72e986273164e9b31bd0d8fd
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-03-16 16:22:05 +0100

    Add documentation for all random number generator classes.

--- localizer/include/random_generators.h
+++ localizer/include/random_generators.h
@@ -1,23 +1,33 @@
 #ifndef RANDOM_VECTOR_GENERATOR_H_
 #define RANDOM_VECTOR_GENERATOR_H_
 
+// Standard template library.
 #include <vector>
 
+// Boost.
 #include <boost/random/normal_distribution.hpp>
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/variate_generator.hpp>
 #include <boost/random/uniform_real.hpp>
 
+// ROS coordinate transformations.
 #include <tf/tf.h>
 
 
+/// Base class for all number generators.
+/// Holds a static member used to seed all derived generator instances.
+/// In this way, the generated random numbers in a program -- even with
+/// many random number generators -- are reproducible.
 class NumberGenerator
 {
 protected:
+    /// Seed value for random number generation.
     static int seed_;
 
 
 protected:
+    /// Constructor.
+    /// Increases the seed value every time it is called.
     NumberGenerator()
     {
         seed_++;
@@ -28,19 +38,26 @@ protected:
 int NumberGenerator::seed_ = 0;
 
 
+/// Generates random numbers sampled from a Gaussian distribution.
 class GaussNumberGenerator : NumberGenerator
 {
 protected:
+    /// Random number generator engine.
     boost::random::variate_generator<boost::mt19937, boost::normal_distribution<double> > generator_;
 
 
 public:
+    /// Constructor.
+    /// Stores the mean and the variance of the
+    /// normal distribution to sample from.
     GaussNumberGenerator(double mean = 0.0, double var = 1.0)
-     : generator_(boost::mt19937(seed_), boost::normal_distribution<double>(mean, var))
+     : generator_(boost::mt19937(seed_),
+                  boost::normal_distribution<double>(mean, std::max(std::numeric_limits<double>::epsilon(), var)))
     {
     }
 
 
+    /// Returns a random number sampled from a Gaussian probability distribution.
     double operator()()
     {
         return generator_();
@@ -48,19 +65,25 @@ public:
 };
 
 
+/// Generates random numbers that are uniformly distributed within the specified interval.
 class UniformNumberGenerator : NumberGenerator
 {
 protected:
+    /// Random number generator engine.
     boost::random::variate_generator<boost::mt19937, boost::uniform_real<double> > generator_;
 
 
 public:
+    /// Constructor.
+    /// Stores the interval the random numbers are sampled from.
     UniformNumberGenerator(double min = 0.0, double max = 1.0)
-        : generator_(boost::mt19937(seed_), boost::uniform_real<double>(min, max))
+        : generator_(boost::mt19937(seed_),
+                     boost::uniform_real<double>(min, std::max(min, max)))
     {
     }
 
 
+    /// Returns a random number from the given interval.
     double operator()()
     {
         return generator_();
@@ -68,13 +91,17 @@ public:
 };
 
 
+/// Generates 3D vectors whose coordinates are sampled from Gaussian distributions.
 class GaussVectorGenerator
 {
 protected:
+    /// Gaussian random number generators for all dimensions.
     std::vector<GaussNumberGenerator> number_generators_;
 
 
 public:
+    /// Constructor.
+    /// Stores the mean and the variance for all dimensions of the vector.
     GaussVectorGenerator(const tf::Vector3& mean, const tf::Vector3& var)
     {
         for (int row = 0; row < 3; row++)
@@ -82,6 +109,7 @@ public:
     }
 
 
+    /// Returns a random vector.
     tf::Vector3 operator()()
     {
         tf::Vector3 vector;
@@ -94,13 +122,17 @@ public:
 };
 
 
+/// Generates random vectors whose coordinates are uniformly distributed.
 class UniformVectorGenerator
 {
 protected:
+    /// Uniform random number generators for all dimensions.
     std::vector<UniformNumberGenerator> number_generators_;
 
 
 public:
+    /// Constructor.
+    /// Stores the intervals for all dimensions of the vector.
     UniformVectorGenerator(const tf::Vector3& min, const tf::Vector3& max)
     {
         for (int row = 0; row < 3; row++)
@@ -108,6 +140,7 @@ public:
     }
 
 
+    /// Returns a random vector.
     tf::Vector3 operator()()
     {
         tf::Vector3 vector;
@@ -120,15 +153,30 @@ public:
 };
 
 
+/// Generates random 3D vectors.
+/// The vector is generated in the following way:
+/// -# Sample an azimuth angle uniformly distributed in [0; 2*PI).
+/// -# Sample a length from a Gaussian distribution with mean \c mean_radius
+///    and variance \c var_radius.
+/// -# Sample a z-coordinate from a Gaussian distribution with mean 0 and
+///    variance \c var_z.
+/// -# Construct the 3D vector from the sampled azimuth angle in the x-y plane,
+///    the radius in the x-y plane, and the z coordinate.
 class VectorPolarGenerator
 {
 protected:
+    /// Random number generator for the vector length projected onto the x-y plane.
     GaussNumberGenerator radius_generator_;
+
+    /// Random number generator for the vector's z-coordinate.
     GaussNumberGenerator z_generator_;
+
+    /// Random number generator for the vector's azimuth angle in the x-y plane.
     UniformNumberGenerator angle_generator_;
 
 
 public:
+    /// Constructor.
     VectorPolarGenerator(double mean_radius, double var_radius,
                          double min_angle, double max_angle,
                          double var_z)
@@ -139,6 +187,7 @@ public:
     }
 
 
+    /// Returns a random vector.
     tf::Vector3 operator()()
     {
         double radius   = radius_generator_();
