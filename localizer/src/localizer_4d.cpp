6dba54f807503bd3b965e31745e86cebb8c9cdc3
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-03-17 08:54:03 +0100

    Change published particle message from MarkerArray to PoseArray.

--- localizer/src/localizer_4d.cpp
+++ localizer/src/localizer_4d.cpp
@@ -1,4 +1,4 @@
-#include <visualization_msgs/MarkerArray.h>
+#include <geometry_msgs/PoseArray.h>
 
 #include "particle_filter.h"
 #include "motion_model_4d.h"
@@ -9,7 +9,7 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "localizer4d");
     ros::NodeHandle node_handle;
     ros::Publisher particle_publisher
-            = node_handle.advertise<visualization_msgs::MarkerArray>("particles", 1u);
+            = node_handle.advertise<geometry_msgs::PoseArray>("particles", 1u);
 
     boost::shared_ptr<MotionModel4d> motion_model
             = boost::shared_ptr<MotionModel4d>(new MotionModel4d());
@@ -27,11 +27,14 @@ int main(int argc, char** argv)
     particle_filter.set_motion_model(motion_model);
     particle_filter.set_n_particles(1e4);
 
-    ros::Rate rate(3);
+    tf::Vector3 translation(0.01, 0.0, 0.0);
+    tf::Quaternion rotation;
+    rotation.setRPY(0.0, 0.0, 0.01);
+    tf::Transform movement(rotation, translation);
+
+    ros::Rate rate(20);
     while (ros::ok())
     {
-        tf::Transform movement(tf::Transform::getIdentity());
-        movement.getOrigin().setX(0.1);
         particle_filter.update_motion(movement);
 
         tf::Transform mean = particle_filter.get_mean();
@@ -41,45 +44,28 @@ int main(int argc, char** argv)
 
         // Do not publish if no one is listening.
         if (particle_publisher.getNumSubscribers() < 1)
+        {
+            rate.sleep();
             continue;
+        }
 
-        // Create an empty message.
-        visualization_msgs::MarkerArray marker_array;
+        // Create a particle cloud message.
+        geometry_msgs::PoseArray pose_array;
 
-        // Fill the marker array.
+        pose_array.header.stamp     = ros::Time::now();
+        pose_array.header.frame_id  = "map";
+
+        // Fill the cloud with particles.
         std::vector<Particle> particles(particle_filter.get_particles());
         for (int i = 0; i < particles.size(); i++)
         {
-            geometry_msgs::Pose marker_pose;
-            tf::poseTFToMsg(particles[i].get_pose(), marker_pose);
+            geometry_msgs::Pose pose;
+            tf::poseTFToMsg(particles[i].get_pose(), pose);
 
-            // Choose the size of the sphere so that it represents
-            // the weight of the particle.
-            geometry_msgs::Vector3 marker_scale;
-            marker_scale.x = std::max(0.15, particles[i].get_weight());
-            marker_scale.y = marker_scale.z = marker_scale.x * 0.1;
-
-            // Color the particles red.
-            std_msgs::ColorRGBA marker_color;
-            marker_color.r = marker_color.a = 0.7f;
-            marker_color.g = marker_color.b = 0.0f;
-
-            visualization_msgs::Marker marker;
-            marker.header.stamp       = ros::Time::now();
-            marker.header.frame_id    = "map";
-            marker.id                 = i;
-            marker.type               = visualization_msgs::Marker::ARROW;
-            marker.action             = visualization_msgs::Marker::ADD;
-            marker.pose               = marker_pose;
-            marker.scale              = marker_scale;
-            marker.color              = marker_color;
-            marker.lifetime           = ros::Duration(10.0);
-            marker.frame_locked       = true;
-
-            marker_array.markers.push_back(marker);
+            pose_array.poses.push_back(pose);
         }
 
-        particle_publisher.publish(marker_array);
+        particle_publisher.publish(pose_array);
 
         ros::spinOnce();
 
