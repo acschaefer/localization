197ffa8edeba5f9f378b7ed07d1894ea01eab078
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-09-21 10:04:31 +0200

    Finish implementation.

--- localizer/include/localizer/sensor_model_elevation.h
+++ localizer/include/localizer/sensor_model_elevation.h
@@ -2,10 +2,7 @@
 #define SENSOR_MODEL_ELEVATION_H_ SENSOR_MODEL_ELEVATION_H_
 
 // Enable/disable multithreading.
-#define MULTITHREADING true
-
-// Enable/disable saving PCD files for debugging.
-#define SAVE_PCD false
+#define MULTITHREADING false
 
 // Standard library.
 #include <vector>
@@ -17,50 +14,41 @@
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
 #include <pcl/filters/voxel_grid.h>
-#include <pcl/kdtree/kdtree_flann.h>
 
 // ROS.
 #include <ros/console.h>
 #include <pcl_ros/transforms.h>
+
+// Elevation map.
+#include "elevation_map.h"
 
 // Particle filter.
 #include "localizer/particle.h"
 #include "localizer/sensor_model.h"
 
 
-/// Determines the weight of a particle by comparing a given point cloud to a point cloud map using the
-/// endpoint model.
-class SensorModelEndpoint : public SensorModel<pcl::PointCloud<pcl::PointXYZI> >
+/// Determines the weight of a particle by comparing two elevation maps: one generated from the global map and one
+/// generated from the robot's measurements.
+class SensorModelElevation : public SensorModel<pcl::PointCloud<pcl::PointXYZI> >
 {
 protected:
-    /// 3D tree for computing the distances between points.
-    pcl::KdTreeFLANN<pcl::PointXYZI> kdtree_;
-
-    /// Voxel edge length used for sparsifying the incoming point clouds.
+    /// Resolution of the downsampled point clouds.
     double res_;
 
     /// Lower bound of the resolution used to sparsify point clouds.
     static const double min_res;
 
+    /// Elevation map corresponding to global map.
+    ElevationMap<pcl::PointXYZI> map_;
+
 
 public:
     /// Constructor.
-    /// \param[in] PCD file used as a map when weighting the particles.
-    SensorModelEndpoint(pcl::PointCloud<pcl::PointXYZI>::ConstPtr map, double res = min_res)
-        : res_(min_res)
+    /// \param[in] PCD file that is converted to the global elevation map.
+    SensorModelElevation(const pcl::PointCloud<pcl::PointXYZI>& map, double res = min_res)
+        : map_(map, res),
+          res_(map_.resolution())
     {
-        set_sparsification_resolution(res);
-
-        // Downsample map and pass it to nearest-neighbor algorithm.
-        pcl::PointCloud<pcl::PointXYZI>::Ptr map_sparse(new pcl::PointCloud<pcl::PointXYZI>());
-        sparsify(*map, *map_sparse);
-        kdtree_.setInputCloud(map_sparse);
-
-        if (SAVE_PCD)
-        {
-            pcl::io::savePCDFileASCII("map.pcd", *map);
-            ROS_DEBUG("Saved \"map.pcd\".");
-        }
     }
 
 
@@ -83,7 +71,7 @@ public:
             for (int t = 0; t < n_threads; t++)
             {
                 threads.create_thread(boost::bind(
-                                          &SensorModelEndpoint::compute_particle_weights_thread,
+                                          &SensorModelElevation::compute_particle_weights_thread,
                                           this,
                                           boost::cref(pc_sparse), boost::ref(particles), t));
             }
@@ -101,17 +89,6 @@ public:
 
 
 protected:
-    /// Set the resolution used for sparsifying the map and incoming lidar scans.
-    void set_sparsification_resolution(double res)
-    {
-        // Make sure the voxel edge length used for sparsification is not smaller than allowed.
-        if (res < min_res)
-            ROS_WARN_STREAM("Sparsification resolution set to minimum admissible resolution " << min_res << ".");
-
-        res_ = std::max(min_res, res);
-    }
-
-
     /// Computes the weights of a subset of particles when using multiple threads.
     /// \param[in,out] particles vector of all particles.
     /// \param[in] pc_robot lidar point cloud in the robot frame of reference.
@@ -148,49 +125,23 @@ protected:
     virtual void compute_particle_weight(const pcl::PointCloud<pcl::PointXYZI>& pc_robot, Particle& particle)
     {
         // Set the maximum distance between two points used for weighting the particles.
-        const float d_max = 0.5f;
+        const double d_max = 0.5;
 
         // Transform the sensor point cloud from the particle frame to the map frame.
         pcl::PointCloud<pcl::PointXYZI> pc_map;
         pcl_ros::transformPointCloud(pc_robot, pc_map, particle.pose);
 
-        // Compute how well the measurements match the map by computing the point-to-point distances.
-        std::vector<int> k_indices(1, -1);
-        std::vector<float> d(1, 0.0f);
-        float d_tot = 0.0f;
-        int n_tot = 0;
-        for (size_t i = 0; i < pc_map.size(); ++i)
-        {
-            // Determine the squared distance to the nearest point.
-            if (pcl::isFinite(pc_map[i]))
-                kdtree_.nearestKSearch(pc_map[i], 1, k_indices, d);
-            else
-                continue;
+        // Convert the point cloud to an elevation map.
+        ElevationMap<pcl::PointXYZI> map_robot(pc_robot, map_.resolution());
 
-            // Sum up the capped distances.
-            d_tot += std::min(d_max, std::sqrt(d[0]));
-
-            // Sum up the number of finite points.
-            n_tot++;
-        }
-
-        // Compute the particle weight relative to the baseline.
-        particle.weight = d_max - d_tot/n_tot;
-
-        // Save the point clouds for debugging reasons.
-        if (SAVE_PCD)
-        {
-            std::stringstream filename;
-            ros::Time now(ros::Time::now());
-            filename << now.sec << now.nsec << ".pcd";
-            pcl::io::savePCDFileASCII(filename.str(), pc_map);
-            ROS_DEBUG_STREAM("Saved \"" << filename.str() << "\".");
-        }
+        // Compute how well the measurements match the map by computing the mean distance between the tiles
+        // of the elevation maps.
+        particle.weight = d_max - map_robot.diff(map_);
     }
 };
 
 
-const double SensorModelEndpoint::min_res = 1.0e-3;
+const double SensorModelElevation::min_res = 1.0e-3;
 
 
 #endif
