e9bdbdab22d086bc5195b6f68e1dd9cc6971fc9e
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-09-21 11:12:33 +0200

    Make sure the map contains at least one tile.

--- localizer/include/localizer/elevation_map.h
+++ localizer/include/localizer/elevation_map.h
@@ -59,8 +59,8 @@ public:
         y_min_ = std::floor(y_min/resolution) * resolution_;
 
         // Compute the size of the map.
-        size_t x_size = std::ceil((x_max-x_min_) / resolution_);
-        size_t y_size = std::ceil((y_max-y_min_) / resolution_);
+        size_t x_size = std::max(std::ceil((x_max-x_min_) / resolution_), 1);
+        size_t y_size = std::max(std::ceil((y_max-y_min_) / resolution_), 1);
 
         // Allocate the map and set all values to NaN.
         map_.resize(x_size);
