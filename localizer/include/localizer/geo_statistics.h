7fcfe40c8404bfa2a3784e2bab8f1168165fab06
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-11-14 12:57:13 +0100

    Add documentation.

--- localizer/include/localizer/geo_statistics.h
+++ localizer/include/localizer/geo_statistics.h
@@ -1,25 +1,33 @@
 #ifndef GEO_STATISTICS_H
 #define GEO_STATISTICS_H
 
+// Standard libraries.
 #include <vector>
 #include <numeric>
+
+// ROS coordinate transformations and logging.
 #include <tf/tf.h>
-#include <Eigen/Eigenvalues>
 #include <ros/console.h>
 
+// Eigen.
+#include <Eigen/Eigenvalues>
 
+
+/// Computes the mean of all given vectors.
 tf::Vector3 vectormean(const std::vector<tf::Vector3>& v)
 {
     return std::accumulate(v.begin(), v.end(), tf::Vector3(0.0, 0.0, 0.0)) / v.size();
 }
 
 
+/// Scales the given vectors by the given scaling factor.
 tf::Vector3 vectorscale(const tf::Vector3& v, const double& s)
 {
     return v * s;
 }
 
 
+/// Computes the weighted average of all given vectors using the given weight factors.
 tf::Vector3 vectormean(std::vector<tf::Vector3> v, const std::vector<double>& w)
 {
     ROS_ERROR_COND(v.size() != w.size(), "Numbers of vectors and weights do not match.");
@@ -30,6 +38,11 @@ tf::Vector3 vectormean(std::vector<tf::Vector3> v, const std::vector<double>& w)
 }
 
 
+/// Compute the weighted average of all given quaternions using the given weight factors.
+/// This method uses the quaternion averaging approach proposed by Markley:
+/// F. Landis Markley, Yang Cheng, John Lucas Crassidis, and Yaakov Oshman.
+/// Averaging Quaternions.
+/// Journal of Guidance, Control, and Dynamics, 30(4):1193-1197, 2007.
 tf::Quaternion quatmean(const std::vector<tf::Quaternion>& q, const std::vector<double>& w)
 {
     Eigen::Matrix4d M = Eigen::Matrix4d::Zero();
@@ -50,12 +63,14 @@ tf::Quaternion quatmean(const std::vector<tf::Quaternion>& q, const std::vector<
 }
 
 
+/// Compute the mean of all given quaternions.
 tf::Quaternion quatmean(const std::vector<tf::Quaternion>& q)
 {
     return quatmean(q, std::vector<double>(q.size(), 1.0/q.size()));
 }
 
 
+/// Compute the weighted average of all given transformations using the given weight factors.
 tf::Transform tfmean(const std::vector<tf::Transform>& tf, const std::vector<double>& w)
 {
     std::vector<tf::Vector3> v;
@@ -70,6 +85,7 @@ tf::Transform tfmean(const std::vector<tf::Transform>& tf, const std::vector<dou
 }
 
 
+/// Compute the average of all given transformations.
 tf::Transform tfmean(const std::vector<tf::Transform>& tf)
 {
     return tfmean(tf, std::vector<double>(tf.size(), 1.0/tf.size()));
